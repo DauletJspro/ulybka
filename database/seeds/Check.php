78adc7612bc0cb79590c9e8e0744cc5b73d543f4
daulet <sailauovdaulet15@gmail.com>
2020-12-23 14:14:45 +0500

    change place on the table

--- database/seeds/Check.php
+++ database/seeds/Check.php
@@ -16,9 +16,13 @@ class Check extends Seeder
             $body_structure = \App\Models\StructureBody::where(['id' => 3])->first();
             $tree = $body_structure->tree_representation;
             $tree = json_decode($tree);
-            $idx = array_search(252, $tree);
+            $idx = array_search(109, $tree);
             $left_child_idx = app(BinaryStructureController::class)->get_left_child_idx($idx);
-            $tree[$left_child_idx] = 296;
+            $tree[$left_child_idx] = 105;
+
+            $idx = array_search(106, $tree);
+            $left_child_idx = app(BinaryStructureController::class)->get_left_child_idx($idx);
+            $tree[$left_child_idx] = 370;
 
             $body_structure->tree_representation = json_encode($tree);
             $body_structure->save();
