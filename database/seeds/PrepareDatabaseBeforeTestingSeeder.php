5d7b578c57d9bfe57d74ae9c96d34816b6346837
AminDiyasSerikuly <amindiyass@gmail.com>
2020-12-30 10:49:46 +0600

    refresh database

--- database/seeds/PrepareDatabaseBeforeTestingSeeder.php
+++ database/seeds/PrepareDatabaseBeforeTestingSeeder.php
@@ -13,13 +13,13 @@ class PrepareDatabaseBeforeTestingSeeder extends Seeder
     {
         $users = \App\Models\Users::all();
 
-//        foreach ($users as $user) {
-//            $user->user_money = 0;
-//            $user->product_balance = 0;
-//            $user->status_id = 0;
-//            $user->password = \Illuminate\Support\Facades\Hash::make('123456');
-//            $user->save();
-//        }
+        foreach ($users as $user) {
+            $user->user_money = 0;
+            $user->product_balance = 0;
+            $user->status_id = 0;
+            $user->password = \Illuminate\Support\Facades\Hash::make('123456');
+            $user->save();
+        }
 
         $user_packets = \App\Models\UserPacket::where(['is_active' => true])->get();
         foreach ($user_packets as $user_packet) {
