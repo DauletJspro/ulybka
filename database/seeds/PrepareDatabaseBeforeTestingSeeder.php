9406e7fb1dbbd6105343881fdbf8a86a9bc2cedd
AminDiyasSerikuly <amindiyass@gmail.com>
2021-01-26 10:09:17 +0600

    make zero all users

--- database/seeds/PrepareDatabaseBeforeTestingSeeder.php
+++ database/seeds/PrepareDatabaseBeforeTestingSeeder.php
@@ -12,24 +12,12 @@ class PrepareDatabaseBeforeTestingSeeder extends Seeder
      */
     public function run()
     {
-//        $user = Users::where(['user_id' => 1])->first();
-//        $user->password = \Illuminate\Support\Facades\Hash::make('123456');
-//        $user->save();
 
         $users = \App\Models\Users::all();
 
         foreach ($users as $user) {
             $user->user_money = 0;
-            $user->product_balance = 0;
-            $user->status_id = 0;
-            $user->password = \Illuminate\Support\Facades\Hash::make('123456');
             $user->save();
-        }
-
-        $user_packets = \App\Models\UserPacket::where(['is_active' => true])->get();
-        foreach ($user_packets as $user_packet) {
-            $user_packet->is_active = false;
-            $user_packet->save();
         }
     }
 }
